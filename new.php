497ae92d32fe9950005444088823e9c77ef2b605
free20064u <free20064u@gmail.com>
2021-03-18 10:20:57 +0000

    12

--- new.php
+++ new.php
@@ -1,8 +1,1 @@
 <?php
-
-$vars = array('name' => 'Oscar the Grouch',
-              'color' => 'green',
-              'favorite_punctuation' => '#');
-
-$query_string = http_build_query($var);
-$url = 'muppetselect.php?' . $query_string;
